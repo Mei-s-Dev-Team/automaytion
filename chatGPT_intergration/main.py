5d1d05369a1fee5a24111e498dc64a7d53a11e84
Evelyn Zhang <zyh991001@gmail.com>
2023-03-07 19:43:35 -0800

    Adding file output from chatGPT

--- chatGPT_intergration/main.py
+++ chatGPT_intergration/main.py
@@ -14,22 +14,23 @@ def main():
         
         #run, chrome.exe %link% " --new-window "
 
-        f.write('run, ')
-        f.write('chrome.exe ')
-        f.write('google.com ')
-        f.write('" --new-window "')
+        # f.write('run, ')
+        # f.write('chrome.exe ')
+        # f.write('google.com ')
+        # f.write('" --new-window "')
         #os.system('cmd /k AutoHotKey.exe')
-        f. close()
+        
         
 
         completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", 
         messages=[{"role": "user", "content":\
-                  "Tell the world about\
-                   the ChatGPT API in the style of a pirate."}]
+                  "write a random ahk script"}]
         )
 
-        print(completion)
+        print(completion['choices'][0]['message']['content'])
+        f.write(completion['choices'][0]['message']['content'])
+        f. close()
 
 if __name__ == '__main__':
     main()
--- exec.ahk
+++ exec.ahk
@@ -1,1 +1,30 @@
-run, chrome.exe google.com " --new-window "
+
+
+#NoEnv ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #Warn ; Enable warnings to assist with detecting common errors.
+SendMode Input ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir% ; Ensures a consistent starting directory.
+
+; This script opens the Google homepage and searches for the word "AutoHotkey"
+#SingleInstance force
+
+; Open Google Homepage
+Run, www.google.com
+
+; Wait for 2 seconds to let the website load
+Sleep, 2000
+
+; Type the word "AutoHotkey" into the search bar
+Send, AutoHotkey
+
+; Wait for 1 second
+Sleep, 1000
+
+; Press the Enter key to perform the search
+Send, {Enter}
+
+; Wait for 3 seconds to let the search results load
+Sleep, 3000
+
+; Scroll down the page to view more search results
+Send, {PgDn 3}
