7e42cba84348428bc2da2b17c4bf590873c56f1e
Evelyn Zhang <zyh991001@gmail.com>
2023-03-20 16:35:01 -0700

    Disabling voice for now and using typing input and switching to powershell scripts

--- chatGPT_intergration/main.py
+++ chatGPT_intergration/main.py
@@ -1,5 +1,13 @@
 import os
 import openai
+from dotenv import load_dotenv
+import speech_recognition as sr
+import sounddevice as sd
+import soundfile as sf
+from scipy.io.wavfile import write
+
+
+
 code_type = "ahk"
 
 #['run', 'chrome.exe ', '']
@@ -7,30 +15,57 @@ code_type = "ahk"
 #avail_options[run] = {exeFile, input, flags}
 avail_options = dict()
 avail_options['run'] = {'exefile':'', 'input':'', 'flags':''}
+load_dotenv()
+
+def voice_commands():
+    r = sr.Recognizer()
+    # Set the duration of the recording in seconds
+    duration = 2
+    
+        # Set the sample rate of the recording
+    sample_rate = 44100
+
+    # Record the audio
+    audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=2)
+
+    # Wait for the recording to finish
+    sd.wait()
+
+    # Save the audio to a WAV file
+    sf.write('recording.wav', audio, sample_rate)
+
+    try:
+        ret = ""
+        input_file = sr.AudioFile('recording.wav')
+        with input_file as source:
+            input_audio = r.record(source)
+            print("Google Speech Recognition thinks you said " + r.recognize_google(input_audio))
+    except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    finally:
+        return ret
+
+
 def main():
+
     if code_type == "ahk":
         print(f"code_type: ahk")
-        f = open("exec.ahk", "w")
+        f = open("exec.ps1", "w")
         
-        #run, chrome.exe %link% " --new-window "
+        prompt = input('Waiting for prompt: ') + "Do not say anything other than the script itself"
 
-        # f.write('run, ')
-        # f.write('chrome.exe ')
-        # f.write('google.com ')
-        # f.write('" --new-window "')
-        #os.system('cmd /k AutoHotKey.exe')
-        
-        
-
+        openai.api_key = os.getenv("OPENAI_KEY")
         completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", 
         messages=[{"role": "user", "content":\
-                  "write a random ahk script"}]
+                  f"{prompt}"}]
         )
 
-        print(completion['choices'][0]['message']['content'])
+        # print(completion['choices'][0]['message']['content'])
         f.write(completion['choices'][0]['message']['content'])
-        f. close()
+        f.close()
 
 if __name__ == '__main__':
     main()
